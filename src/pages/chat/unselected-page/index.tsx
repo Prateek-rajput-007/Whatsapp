2bf0971c208021260045817526ffe0637d80c2a5
Prateek-rajput-007 <prateekrajput47@yahoo.com>
2025-05-22 19:45:34 +0530

    first commit

--- src/pages/chat/unselected-page/index.tsx
+++ src/pages/chat/unselected-page/index.tsx
@@ -24,8 +24,8 @@ export default function UnSelectedChatPage() {
         </Text>
         <Text>
           <span>Built by</span>{" "}
-          <Link target="_blank" href="https://github.com/jazimabbas">
-            Jazim Abbas
+          <Link target="_blank" href="https://github.com/Prateek-rajput-007">
+            Prateek
           </Link>
           <IconWrapper>
             <Icon id="heart" />
